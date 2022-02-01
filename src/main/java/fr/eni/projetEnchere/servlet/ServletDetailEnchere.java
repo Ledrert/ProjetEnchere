9530006945162cd86689c84bfdaa746d7cb58a02
bgarniel2021 <bgarniel2021@177SE44-9JZ355J.ad.campus-eni.fr>
2022-02-01 16:49:55 +0100

    Ajout JSP/Servlet DetailEnchere

---
+++ src/main/java/fr/eni/projetEnchere/servlet/ServletDetailEnchere.java
@@ -0,0 +1,42 @@
+package fr.eni.projetEnchere.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletDetailEnchere
+ */
+@WebServlet(name="/ServletDetailEnchere", urlPatterns = "/detailEnchere")
+public class ServletDetailEnchere extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletDetailEnchere() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/DetailEnchere.jsp");
+		rd.forward(request, response);
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
