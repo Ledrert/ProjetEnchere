15c51c2a273d054d43e8d585fa6d951a1d4a2981
Antoine <PC@DESKTOP-H8KFLEQ>
2022-02-02 14:16:16 +0100

    Modif Servlet Connexion/Inscription + UtilisateurManager pour crée user

--- src/main/java/fr/eni/projetEnchere/bll/UtilisateurManager.java
+++ src/main/java/fr/eni/projetEnchere/bll/UtilisateurManager.java
@@ -1,5 +1,10 @@
 package fr.eni.projetEnchere.bll;
 
+import fr.eni.projetEnchere.bo.Utilisateur;
+import fr.eni.projetEnchere.dal.DAOFactory;
+import fr.eni.projetEnchere.dal.DalException;
+import fr.eni.projetEnchere.dal.UtilisateurDAO;
+
 public class UtilisateurManager {
 
 	private static UtilisateurManager instance;
@@ -11,9 +16,16 @@ public class UtilisateurManager {
 		return instance;
 	}
 	
-	private UtilisateurManager() {
-		
+	private UtilisateurManager() {	
 	}
+
+	public void addUtilisateur(String pseudo, String nom, String prenom, String email, String rue, String codePostal, 
+			String ville, String password) throws DalException {
+		UtilisateurDAO ud = DAOFactory.getUtilisateurDAO();
+		Utilisateur utilisateur = new Utilisateur(pseudo, nom, prenom, email, rue, codePostal, ville, password, false);
+		ud.AjouterUtilisateur(utilisateur);
+	}
+
 	
 	
 }
--- src/main/java/fr/eni/projetEnchere/servlet/ServletInscription.java
+++ src/main/java/fr/eni/projetEnchere/servlet/ServletInscription.java
@@ -9,6 +9,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import fr.eni.projetEnchere.bll.UtilisateurManager;
+import fr.eni.projetEnchere.dal.DalException;
+
 /**
  * Servlet implementation class ServletModifProfil
  */
@@ -16,7 +19,6 @@ import javax.servlet.http.HttpServletResponse;
 public class ServletInscription extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
-
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
@@ -29,8 +31,22 @@ public class ServletInscription extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String pseudo = request.getParameter("pseudo");
+		String nom = request.getParameter("nom");
+		String prenom = request.getParameter("prenom");
+		String email = request.getParameter("email");
+		String rue = request.getParameter("rue");
+		String codePostal = request.getParameter("codePostal");
+		String ville = request.getParameter("ville");
+		String password = request.getParameter("password"); // Hash ??
+				
+		UtilisateurManager um = UtilisateurManager.getInstance();
+		try {
+			um.addUtilisateur(pseudo, nom, prenom, email, rue, codePostal, ville, password);
+		} catch (DalException e) {
+			e.printStackTrace();
+		}
+		response.sendRedirect(request.getContextPath()+"/login");
 		doGet(request, response);
 	}
-
 }
